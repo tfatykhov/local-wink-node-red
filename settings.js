0402fb417dacb10e84bd1fbeda8bffa4f65ba580
tfatykhov <tfatykhov@gmail.com>
2017-01-07 11:24:39 -0500

    modified settings

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014, 2015 IBM Corp.
+ * Copyright 2013, 2015 IBM Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,79 +14,216 @@
  * limitations under the License.
  **/
 
+// The `https` setting requires the `fs` module. Uncomment the following
+// to make it available:
+//var fs = require("fs");
+var fs = require("fs");
 var path = require("path");
+var NODE_RED_USERNAME="XXXXX";
+var NODE_RED_PASSWORD="XXXXX";
 var when = require("when");
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+module.exports = {
+    // the tcp port that the Node-RED web server is listening on
+    uiPort: 1880,
 
-var cfenv = require("cfenv");
-var appEnv = cfenv.getAppEnv();
+    editorTheme: { menu: { "menu-item-help": {
+        label: "Node-RED Pi Website",
+        url: "http://nodered.org/docs/hardware/raspberrypi.html"
+    } } },
 
-var VCAP_APPLICATION = JSON.parse(process.env.VCAP_APPLICATION);
-var VCAP_SERVICES = JSON.parse(process.env.VCAP_SERVICES);
 
-var settings = module.exports = {
-    uiPort: process.env.PORT || 1880,
+    // By default, the Node-RED UI accepts connections on all IPv4 interfaces.
+    // The following property can be used to listen on a specific interface. For
+    // example, the following would only allow connections from the local machine.
+    //uiHost: "127.0.0.1",
+
+    // Retry time in milliseconds for MQTT connections
     mqttReconnectTime: 15000,
+
+    // Retry time in milliseconds for Serial port connections
     serialReconnectTime: 15000,
+
+    // Retry time in milliseconds for TCP socket connections
+    //socketReconnectTime: 10000,
+
+    // Timeout in milliseconds for TCP server socket connections
+    //  defaults to no timeout
+    //socketTimeout: 120000,
+
+    // Timeout in milliseconds for HTTP request connections
+    //  defaults to 120 seconds
+    //httpRequestTimeout: 120000,
+
+    // The maximum length, in characters, of any message sent to the debug sidebar tab
     debugMaxLength: 1000,
 
-    // Add the bluemix-specific nodes in
-    nodesDir: path.join(__dirname,"nodes"),
+    // The file containing the flows. If not set, it defaults to flows_<hostname>.json
+    //flowFile: 'flows.json',
 
-    // Blacklist the non-bluemix friendly nodes
-    nodesExcludes:['66-mongodb.js','75-exec.js','35-arduino.js','36-rpi-gpio.js','25-serial.js','28-tail.js','50-file.js','31-tcpin.js','32-udp.js','23-watch.js'],
+    // To enabled pretty-printing of the flow within the flow file, set the following
+    //  property to true:
+    //flowFilePretty: true,
 
-    // Enable module reinstalls on start-up; this ensures modules installed
-    // post-deploy are restored after a restage
-    autoInstallModules: true,
+    // By default, all user data is stored in the Node-RED install directory. To
+    // use a different location, the following property can be used
+    //userDir: '/home/nol/.node-red/',
 
-    // Move the admin UI
-    httpAdminRoot: '/red',
+    // Node-RED scans the `nodes` directory in the install directory to find nodes.
+    // The following property can be used to specify an additional directory to scan.
+    //nodesDir: '/home/nol/.node-red/nodes',
 
-    // Serve up the welcome page
-    httpStatic: path.join(__dirname,"public"),
+    // By default, the Node-RED UI is available at http://localhost:1880/
+    // The following property can be used to specifiy a different root path.
+    // If set to false, this is disabled.
+    //httpAdminRoot: '/admin',
 
-    functionGlobalContext: { },
+    // Some nodes, such as HTTP In, can be used to listen for incoming http requests.
+    // By default, these are served relative to '/'. The following property
+    // can be used to specifiy a different root path. If set to false, this is
+    // disabled.
+    //httpNodeRoot: '/red-nodes',
 
-    storageModule: require("./couchstorage")
-}
-// You can set adminAuth yourself in this file, but it will look for the
-// the following environment variables and automatically enable adminAuth
-// if they have been set. That means you don't have to hardcode any
-// credentials in this file.
-if (process.env.NODE_RED_USERNAME && process.env.NODE_RED_PASSWORD) {
-    settings.adminAuth = {
-        type: "credentials",
-        users: function(username) {
-            if (process.env.NODE_RED_USERNAME == username) {
-                return when.resolve({username:username,permissions:"*"});
-            } else {
-                return when.resolve(null);
-            }
-        },
-        authenticate: function(username, password) {
-            if (process.env.NODE_RED_USERNAME == username &&
-                process.env.NODE_RED_PASSWORD == password) {
-                return when.resolve({username:username,permissions:"*"});
-            } else {
-                return when.resolve(null);
-            }
+    // The following property can be used in place of 'httpAdminRoot' and 'httpNodeRoot',
+    // to apply the same root to both parts.
+    //httpRoot: '/red',
+
+    // When httpAdminRoot is used to move the UI to a different root path, the
+    // following property can be used to identify a directory of static content
+    // that should be served at http://localhost:1880/.
+    //httpStatic: '/home/nol/node-red-dashboard/',
+
+    // Securing Node-RED
+    // -----------------
+    // To password protect the Node-RED editor and admin API, the following
+    // property can be used. See http://nodered.org/docs/security.html for details.
+    //adminAuth: {
+    //    type: "credentials",
+    //    users: [{
+    //        username: "admin",
+    //        password: "$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN.",
+    //        permissions: "*"
+    //    }]
+    //},
+
+    // To password protect the node-defined HTTP endpoints (httpNodeRoot), or
+    // the static content (httpStatic), the following properties can be used.
+    // The pass field is a bcrypt hash of the password.
+    // See http://nodered.org/docs/security.html#generating-the-password-hash
+    //httpNodeAuth: {user:"user",pass:"$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN."},
+    //httpStaticAuth: {user:"user",pass:"$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN."},
+
+    // The following property can be used to enable HTTPS
+    // See http://nodejs.org/api/https.html#https_https_createserver_options_requestlistener
+    // for details on its contents.
+    // See the comment at the top of this file on how to load the `fs` module used by
+    // this setting.
+    //
+    //https: {
+    //    key: fs.readFileSync('privatekey.pem'),
+    //    cert: fs.readFileSync('certificate.pem')
+    //},
+
+    // The following property can be used to disable the editor. The admin API
+    // is not affected by this option. To disable both the editor and the admin
+    // API, use either the httpRoot or httpAdminRoot properties
+    //disableEditor: false,
+
+    // The following property can be used to configure cross-origin resource sharing
+    // in the HTTP nodes.
+    // See https://github.com/troygoode/node-cors#configuration-options for
+    // details on its contents. The following is a basic permissive set of options:
+    //httpNodeCors: {
+    //    origin: "*",
+    //    methods: "GET,PUT,POST,DELETE"
+    //},
+
+    // If you need to set an http proxy please set an environment variable
+    // called http_proxy (or HTTP_PROXY) outside of Node-RED in the operating system.
+    // For example - http_proxy=http://myproxy.com:8080
+    // (Setting it here will have no effect)
+    // You may also specify no_proxy (or NO_PROXY) to supply a comma separated
+    // list of domains to not proxy, eg - no_proxy=.acme.co,.acme.co.uk
+
+    // The following property can be used to add a custom middleware function
+    // in front of all http in nodes. This allows custom authentication to be
+    // applied to all http in nodes, or any other sort of common request processing.
+    //httpNodeMiddleware: function(req,res,next) {
+    //   // Handle/reject the request, or pass it on to the http in node
+    //   // by calling next();
+    //   next();
+    //},
+
+    // Anything in this hash is globally available to all functions.
+    // It is accessed as context.global.
+    // eg:
+    //    functionGlobalContext: { os:require('os') }
+    // can be accessed in a function block as:
+    //    context.global.os
+    functionGlobalContext: {
+//	scanner:require('chromecast-scanner'),
+        wnr : require("winknodered")
+	    ,CRYPTO: require("crypto")
+	    ,SunCalc : require("suncalc")
+	    ,wnr : require("winknodered")
+	    ,tinycolor : require("tinycolor2")
+        // os:require('os'),
+        // bonescript:require('bonescript'),
+        // jfive:require("johnny-five"),
+        // j5board:require("johnny-five").Board({repl:false})
+	,WinkUser: {"uid":"XXX","pwd":"XXX"},
+	BlueMixUrlBase: "XXX",
+	forecastIoApiKey: "5XXX",
+	FREEBOARD_TOKEN: "XXX",
+	FreeboardAuthTocken: "XXX",
+	IFTTT_TOKEN: "XXX",
+	HomeLocation: { "lat":"XXX.XXX","lon":".XXX.XXX"},
+
+    },
+
+    // The following property can be used to order the categories in the editor
+    // palette. If a node's category is not in the list, the category will get
+    // added to the end of the palette.
+    // If not set, the following default order is used:
+    //paletteCategories: ['subflows', 'input', 'output', 'function', 'social', 'mobile', 'storage', 'analysis', 'advanced'],
+
+    // Configure the logging output
+    logging: {
+        // Only console logging is currently supported
+        console: {
+            // Level of logging to be recorded. Options are:
+            // fatal - only those errors which make the application unusable should be recorded
+            // error - record errors which are deemed fatal for a particular request + fatal errors
+            // warn - record problems which are non fatal + errors + fatal errors
+            // info - record information about the general running of the application + warn + error + fatal errors
+            // debug - record information which is more verbose than info + info + warn + error + fatal errors
+            // trace - record very detailed logging + debug + info + warn + error + fatal errors
+            level: "info",
+
+            // Whether or not to include metric events in the log output
+            metrics: false,
+            // Whether or not to include audit events in the log output
+            audit: false
         }
     }
 }
 
-settings.couchAppname = VCAP_APPLICATION['application_name'];
-
-// NODE_RED_STORAGE_NAME is automatically set by this applications manifest.
-var storageServiceName = process.env.NODE_RED_STORAGE_NAME || new RegExp("^"+settings.couchAppname+".cloudantNoSQLDB");
-var couchService = appEnv.getService(storageServiceName);
-
-if (!couchService) {
-    console.log("Failed to find Cloudant service");
-    if (process.env.NODE_RED_STORAGE_NAME) {
-        console.log(" - using NODE_RED_STORAGE_NAME environment variable: "+process.env.NODE_RED_STORAGE_NAME);
+if (NODE_RED_USERNAME && NODE_RED_PASSWORD) {
+    settings.adminAuth = {
+       type: "credentials",
+        users: function(username) {
+            if (username == NODE_RED_USERNAME) {
+                return when.resolve({username:username,permissions:"*"});
+            } else {
+                return when.resolve({username:username,permissions:"*"});
+            }
+        },
+        authenticate: function(username, password) {
+            if (username==NODE_RED_USERNAME && password==NODE_RED_PASSWORD) {
+                return when.resolve({username:username,permissions:"*"});
+            } else {
+                return when.resolve({username:username,permissions:"*"});
+            }
+        }
     }
-    throw new Error("No cloudant service found");
-}    
-settings.couchUrl = couchService.credentials.url;
-
-
+}
